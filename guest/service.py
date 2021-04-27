083da4cf3e0c977c7e7f888349749a0d9df493ac
wumq <759823845@qq.com>
2021-04-27 17:39:14 +0800

    update

--- guest/service.py
+++
@@ -1,9 +0,0 @@
-from management import my_lib
-from management.models import ProductManage
-
-# Create your service here.
-
-
-def find_order(order_id, token):
-    item = ProductManage.objects.filter(order_id=order_id, token=token).first()
-    return item
