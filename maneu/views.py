4de7e58f861adf01d09fa128e10f2fae3bccac3d
wumingqing2018 <759823845@qq.com>
2022-10-08 01:42:43 +0800

    update

--- maneu/views.py
+++ maneu/views.py
@@ -71,7 +71,7 @@ def test1(request):
         orderCountList['cur_month'] = ten
         orderCountList['yest_month'] = nine
 
-        service.ManeuDatalogs_getorcreate(user_id=user_id, time=common.today(), order_log=json.dumps(orderCountList))
+        dataLogs = service.ManeuDatalogs_getorcreate(user_id=user_id, time=common.today(), order_log=json.dumps(orderCountList))
     return render(request, 'maneu/test1.html', {'dataLogs': json.loads(dataLogs.order_log)})
 
 
