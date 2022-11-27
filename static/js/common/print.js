2a3607839a71063cffb8dfa1a07d4b562188c880
wumingqing2018 <50645992+wumingqing2018@users.noreply.github.com>
2022-11-27 22:08:18 +0800

    update

--- static/js/common/print.js
+++ static/js/common/print.js
@@ -1,15 +1,6 @@
-$(".button").on("click", function(event) {
-    event.preventDefault();
-    html2canvas(document.getElementById("textArea"), {
-        allowTaint: true,
-        taintTest: false,
-        onrendered: function(canvas) {
-            canvas.id = "mycanvas";
-            //生成base64图片数据
-            var dataUrl = canvas.toDataURL();
-            var newImg = document.createElement("img");
-            newImg.src =  dataUrl;
-            document.body.appendChild(newImg);
+$(document).ready(function(){
+    $("#print_btn").click(function () {
+            $("#textArea").print();
         }
-    });
-});
+    )
+})
