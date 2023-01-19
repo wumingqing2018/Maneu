bb7518ffffa808d7aa0f3fe6d65e8be188665704
wumingqing2018 <50645992+wumingqing2018@users.noreply.github.com>
2023-01-20 02:47:41 +0800

    update

--- static/js/maneu_order/order_insert_pc.js
+++ static/js/maneu_order/order_insert_pc.js
@@ -6,3 +6,14 @@ $('#insert').click(function () {
     order_json = $('#order').serializeJsonStr();
     $('#order_json').val(order_json);
 })
+
+var InputCount=1;
+$("#AddMoreTextBox").click(function (e)
+{
+    InputCount++;
+    $("#Product_Orders_TABLE").append('<tr><td class="col-8" colspan="5"> <div class="input-group input-group-sm mb-2"> <input autocomplete="off" type="text" name="arg'+ InputCount +'0" class="form-control" style="width: 12%"> <input autocomplete="off" type="text" name="arg'+ InputCount +'1" class="form-control" style="width: 12%"> <input autocomplete="off" type="text" name="arg'+ InputCount +'2" class="form-control" style="width: 12%"> <input autocomplete="off" type="text" name="arg'+ InputCount +'3" class="form-control" style="width: 52%"> <input autocomplete="off" type="text" name="arg'+ InputCount +'4" class="form-control" style="width: 12%"></div> </td></tr>');
+});
+$('#deleteTextBox').click(function (e) {
+    $("#Product_Orders_TABLE>tr").last().remove()
+})
+
