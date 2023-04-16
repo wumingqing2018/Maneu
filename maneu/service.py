689be0df9ed134fa21947f8c9e05e7070c34a0c6
wumingqing2018 <759823845@qq.com>
2023-04-16 23:36:22 +0800

    upgrade

--- maneu/service.py
+++
@@ -1,8 +0,0 @@
-from maneu.models import ManeuAdmin
-
-
-def find_user_username(username=''):
-    """
-    通过username查找用户
-    """
-    return ManeuAdmin.objects.filter(username=username).first()
--- maneu_index/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from maneu_index import views
-
-app_name = 'maneu_index'
-urlpatterns = [
-    # views
-    path('index/', views.index, name='index'),
-]
