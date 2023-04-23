7d4f0a37795770aacd240b139c4d682613d91bb9
wumingqing2018 <759823845@qq.com>
2023-04-23 15:37:31 +0800

    update

--- maneu/settings.py
+++ maneu/settings.py
@@ -103,7 +103,7 @@ https:#docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'maneu',
+        'NAME': 'maneu1',
         'HOST': 'localhost',
         'PORT': 3306,
         'USER': 'root',
