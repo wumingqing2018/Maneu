472837d3ed8f335ed1fd511f8d70c7d82810e4ea
wumq <759823845@qq.com>
2024-10-27 00:35:08 +0800

    代码重整

--- static/javascript/maneu_order/index/time_index.js
+++ static/javascript/maneu_order/index/time_index.js
@@ -12,7 +12,41 @@ $(function () {
                 end: end.format('YYYY-MM-DD 23:59:59')
             },
             success: function (res){
-                console.log(res)
+                data = res.data
+                console.log(data)
+                for (i in data) {
+                    $('#body').append(
+                        "<tr>\n" +
+                        "    <td valign='middle'>\n" +
+                        "        <span>" + res[i]['time'] + "</span>\n" +
+                        "    </td>\n" +
+                        "    <td valign='middle'>\n" +
+                        "        <span>" + res[i]['name'] + "</span>\n" +
+                        "    </td>\n" +
+                        "    <td valign='middle'>\n" +
+                        "        <span>" + res[i]['phone'] + "</span>\n" +
+                        "    </td>\n" +
+                        "    <td valign='middle'>\n" +
+                        "        <span>" + res[i]['remark'] + "</span>\n" +
+                        "    </td>\n" +
+                        "    <td valign='middle' align='right'>\n" +
+                        "        <div class='col-6 row'>\n" +
+                        "            <form class='col-6'>\n" +
+                        "                <div class='col-6 input-group input-group-sm'>\n" +
+                        "                    <input type='button' class='btn btn-danger col-12' onclick='deleteBtn(this)' alt=" + res[i]['id'] + " value='删除'>\n" +
+                        "                </div>\n" +
+                        "            </form>\n" +
+                        "            <form class='col-6' method='get' action=" + api_detail + " >\n" +
+                        "                <input type='hidden' name='order_id' value=" + res[i]['id'] + ">\n" +
+                        "                <div class='input-group input-group-sm'>\n" +
+                        "                    <input type='submit' class='btn btn-primary col-12' value='查看'>\n" +
+                        "                </div>\n" +
+                        "            </form>\n" +
+                        "        </div>\n" +
+                        "    </td>\n" +
+                        "</tr>\n"
+                    )
+                }
             },
         });
     }
