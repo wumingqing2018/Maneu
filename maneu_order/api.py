d604228565d580fd547164c5791ca9644f69a909
wumq <759823845@qq.com>
2024-10-26 23:22:49 +0800

    代码重整

--- maneu_order/api.py
+++ maneu_order/api.py
@@ -8,9 +8,8 @@ def index(request):
     admin_id = is_uuid(request.session.get('id'))
     start = is_date(request.GET.get('star'))
     end = is_date(request.GET.get('end'))
-    print(request.session.get('id'))
 
-    if start and end and admin_id:
+    if admin_id:
         data = service.ManeuOrder_index(admin_id=admin_id, star=start, end=end).values('id', 'name', 'phone', 'time', 'remark')
         content = {'status': True, 'message': '', 'data': list(data)}
     else:
