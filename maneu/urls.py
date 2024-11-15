c14d988ececf0c3acac1b88063c9e5a2667f8d62
wumq <759823845@qq.com>
2024-11-16 00:59:38 +0800

    代码重整

--- maneu/urls.py
+++ maneu/urls.py
@@ -4,20 +4,14 @@ from django.urls import path
 from maneu.views import *
 
 urlpatterns = [
-    # 首页
     path('', index, name='index'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
     path('sendsms/', sendsms, name='sendsms'),
     path('login_api/', login_api, name='login_api'),
-    # 用户子路由
     path('maneu_admin/', include('maneu_admin.urls')),
-    # 客户子路由
     path('maneu_guest/', include('maneu_guest.urls')),
-    # 用户首页子路由
     path('maneu_index/', include('maneu_index.urls')),
-    # 零售订单子路由
     path('maneu_order/', include('maneu_order.urls')),
     path('maneu_report/', include('maneu_report.urls')),
-
 ]
